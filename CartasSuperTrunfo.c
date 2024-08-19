26efa82928988a42c3a5829f230e2e958c2af313
miltonfranciscopf <miltonfranciscopf9@gmail.com>
2024-08-19 15:58:20 +0000

    Começando a criar a forma do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    
+    char estado;
+    char codigoCarta[30];
+    char nomeCidade[30];
+    int populacao;
+    float area;
+    float pib;
+    int numeroPontosTuristicos;
+
     printf("Desafio Cartas Super Trunfos");
     
+
     return 0;
 }
